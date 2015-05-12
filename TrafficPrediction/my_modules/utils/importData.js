4a91b5be7a15b34946e16fb37bd43c4c4ec64723
bkazic <blaz.kazic@ijs.si>
2015-05-12 09:05:54 +0200

    Before touching model.js

---
+++ TrafficPrediction/my_modules/utils/importData.js
@@ -0,0 +1,85 @@
+﻿exports.importData = function (inStores, outStores, limit) {
+    var loadStores = inStores;
+    var targetStores = outStores;
+    var count = 0; //counter used for counting iterations when limit is defined
+    
+    console.log('Importing data from  %s to %s...', 
+        inStores.map(function (store) { return store.name }), 
+        inStores.map(function (store) { return store.name }))
+    
+    // Find and returns first datetime field from store
+    getDateTimeFieldName = function (store) {
+        var dateTimeFieldName = null;
+        for (var ii = 0; ii < store.fields.length; ii++) {
+            if (store.fields[ii].type == "datetime") {
+                dateTimeFieldName = store.fields[ii].name;
+                break;
+            }
+        }
+        return dateTimeFieldName;
+    };
+    
+    // Find and returns all datetime fields in store
+    getDateTimeFieldNames = function (stores) {
+        var result = []
+        for (var ii = 0; ii < stores.length; ii++) {
+            var store = stores[ii];
+            result.push(getDateTimeFieldName(store));
+        }
+        return result;
+    };
+    
+    // Returns index with lowest timestamp value from currRecIdxs array
+    findLowestRecIdx = function (currRecIdxs) {
+        var min = Number.MAX_VALUE;
+        var idx = -1;
+        var dateTimeFields = getDateTimeFieldNames(loadStores);
+        
+        for (var ii = 0; ii < currRecIdxs.length; ii++) {
+            var currRec = loadStores[ii].recs[currRecIdxs[ii]];
+            if (currRec == null) continue;
+            if (currRec[dateTimeFields[ii]].getTime() < min) {
+                min = currRec[dateTimeFields[ii]].getTime();
+                idx = ii;
+            }
+        }
+        return idx;
+    };
+    
+    var currRecIdxs = [];
+    for (var ii = 0; ii < loadStores.length; ii++) {
+        currRecIdxs.push(0);
+    }
+    //var lowestRecIdx = findLowestRecIdx(currRecIdxs);
+    var dateTimeFields = getDateTimeFieldNames(loadStores);
+    while (true) {
+        var lowestRecIdx = findLowestRecIdx(currRecIdxs);
+        if (lowestRecIdx == -1) break;
+        
+        // If input parameter limit is defined
+        if (limit != null) {
+            if (count > limit) {
+                console.log("Reached count limit at " + limit)
+                break;
+            } else count++
+        }
+        
+        //console.log("\nDodal bomo: " + JSON.stringify(loadStores[lowestRecIdx].recs[currRecIdxs[lowestRecIdx]]));
+        
+        var rec = loadStores[lowestRecIdx].recs[currRecIdxs[lowestRecIdx]]
+        var val = rec.toJSON(true);
+        delete val.$id;
+        //val.StringDateTime = rec[dateTimeFields[lowestRecIdx]].string; //have to add string version because, string fields can be uniqe. Thisway I delete duplicates.
+        targetStores[lowestRecIdx].add(val);
+        
+        //console.log("\nLast resampled rec: " + JSON.stringify(resampledStore.last));
+        currRecIdxs[lowestRecIdx]++
+        //console.start()
+    }
+}
+
+// About this module
+exports.about = function () {
+    var description = "Imports data according to timestamp. Instore and outstore are input parameters.";
+    return description;
+};
